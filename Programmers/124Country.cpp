2f3f300de6ec03ce8c1798e03544e190fb73773a
YEAjiJUNG <happyhan96@naver.com>
2020-10-03 20:04:51 +0900

    124Country Problem Solved

---
+++ Programmers/124Country.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    vector <int> v;
+    string arr[3] = {"1", "2", "4"};
+    int num = n;
+    while(n > 0){
+        int r = n % 3;
+        n /= 3;
+        if (r == 0) {
+            answer = arr[2] + answer;
+            n—;
+        } else {
+            answer = arr[r-1] + answer;
+        }
+    }
+    return answer;
+}
