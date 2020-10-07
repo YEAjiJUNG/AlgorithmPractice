737657d8f7335edf0439320c86b9944b6f5b1f01
YEAjiJUNG <happyhan96@naver.com>
2020-10-07 23:27:44 +0900

    StringDown Problem Solved

---
+++ Programmers/StringDown.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = s;
+
+    sort(answer.begin(),answer.end(),greater<char>());
+
+    return answer;
+}
