93973f9b0bd89064408827e895e843e856697880
YEAjiJUNG <happyhan96@naver.com>
2020-11-17 00:02:53 +0900

    PassedBridge Problem Changed

--- Programmers/PassedBridge.cpp
+++ Programmers/PassedBridge.cpp
@@ -1,42 +1,35 @@
 #include <string>
 #include <vector>
 #include <queue>
+
 using namespace std;
- 
-int solution(int bridge_length, int weight, vector<int> truck_weights) 
-{
-    queue<int> q;
+
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
     int time = 0;
     int sum = 0;
-    int truckIdx = 0;
-    
-    while(1)
-    {    
+    int index = 0;
+    queue<int> q;
+
+    while(1){
         time++;
-        
-        if(q.size() == bridge_length)
-        {
+        if(q.size() == bridge_length){
             sum -= q.front();
             q.pop();
         }
-        
-        if(sum + truck_weights[truckIdx] <= weight)
-        {
-            if(truckIdx == truck_weights.size()-1)
-            {
+        if(sum + truck_weights[index] <= weight){
+            if(index == truck_weights.size()-1){
                 time += bridge_length;
                 break;
             }
-            
-            q.push(truck_weights[truckIdx]);
-            sum += truck_weights[truckIdx];
-            truckIdx++;
+
+            q.push(truck_weights[index]);
+            sum += truck_weights[index];
+            index++;
         }
-        else
-        {
+        else{
             q.push(0);
-        }    
+        }
+        
     }
-    
     return time;
 }
