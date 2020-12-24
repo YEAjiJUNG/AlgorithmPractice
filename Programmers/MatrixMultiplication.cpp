08c23d6a14b41cccbb7398555675c5ef0e18cce3
YEAjiJUNG <happyhan96@naver.com>
2020-12-25 00:18:46 +0900

    MatrixMultiplication Problem Updated

--- Programmers/MatrixMultiplication.cpp
+++ Programmers/MatrixMultiplication.cpp
@@ -5,17 +5,22 @@ using namespace std;
 
 vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2)
 {
-    vector<vector<int>> answer(arr1.size(), vector<int>(arr2[0].size(), 0));
-    int cnt = 0;
+    vector<vector<int>> answer;
+
+    int k = 0;
     for (int i = 0; i < arr1.size(); i++)
     {
-        for (int l = 0; l < arr1[0].size(); l++)
+        vector<int> line;
+        for (int j = 0; j < arr2[0].size(); j++)
         {
-            for (int k = 0; k < arr2[0].size(); k++)
+            int sum = 0;
+            for (int k = 0; k < arr1[0].size(); k++)
             {
-                answer[i][k] += arr1[i][l] * arr2[l][k];
+                sum += arr1[i][k] * arr2[k][j];
             }
+            line.push_back(sum);
         }
     }
+
     return answer;
 }
