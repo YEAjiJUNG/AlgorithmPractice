2381503076e87ef39384b6fcf81c474344c501bd
정예지 <yeajijung@jeong-yejiui-MacBookAir.local>
2021-01-28 22:52:19 +0900

    FuctionDevelopment Problem Solved

---
+++ Programmers/FunctionDevelopment.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+    vector<int> rest;
+    queue<int> day;
+    int count = 0;
+
+    for(int i = 0; i < progresses.size(); i++){
+        rest.push_back(100-progresses[i]);
+        if(rest[i] % speeds[i] == 0){
+            day.push(rest[i]/speeds[i]);
+        }
+        else{
+            day.push(rest[i]/speeds[i] + 1);
+        }
+    }
+
+    int comp = -1;
+    while(!day.empty()){
+        int current = day.front();    
+        day.pop();
+        if (comp == -1) {
+            count++;
+            comp = current;    
+            continue;
+        }
+        if (comp < current) {
+            answer.push_back(count);
+            count = 1;
+            comp = current;     
+        } else {
+            count++;
+        }
+        if(day.empty()){
+            answer.push_back(count);
+        }
+    }
+
+    return answer;
+}
